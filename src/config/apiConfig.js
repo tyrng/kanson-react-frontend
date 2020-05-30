5a56cb46c53777afcc6a7b90288e8697b06a97ac
tyrng <ongtypc@gmail.com>
2020-05-30 15:46:57 +0800

    config update

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,3 +1,3 @@
-export const apiUrl = 'http://localhost:44307';
+export const apiUrl = 'https://kanson-netcore-backend.azurewebsites.net';
 
 export const googleClientId = '427748603350-2n6b6kvr76f3a66go5mvqabrs9frr67t.apps.googleusercontent.com';
